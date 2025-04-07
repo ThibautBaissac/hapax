812b7346b9cc92e741d7f61a2c47208ee0af05f2
Thibaut Baissac <tbaissac@proton.me>
2025-04-07 10:44:09 +0200

    Create destroy_all seed action

---
+++ db/seeds/destroy_all.rb
@@ -0,0 +1,19 @@
+puts "Starting to empty the database..."
+
+models = [
+  User
+]
+
+
+ActiveRecord::Base.transaction do
+  models.each do |model|
+    if model.table_exists?
+      puts "Destroying all records of #{model.name}..."
+      model.destroy_all
+    else
+      puts "Skipping #{model.name} as the table does not exist."
+    end
+  end
+end
+
+puts "All tables have been emptied successfully."
