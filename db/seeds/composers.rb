eecbc936431e9caece2f08005a35880bfb83f970
Thibaut Baissac <tbaissac@proton.me>
2025-06-07 14:54:34 +0200

    Update composer seed data to use nationality codes instead of names

--- db/seeds/composers.rb
+++ db/seeds/composers.rb
@@ -5,8 +5,8 @@ puts "🎼 Creating composers"
 composer_count = 0
 
 composers = [
-  { first_name: "Olivier", last_name: "Greif", birth_date: Date.new(1950, 1, 3), death_date: Date.new(2000, 5, 13), nationality: "French" },
-  { first_name: "Ludwig van", last_name: "Beethoven", birth_date: Date.new(1770, 12, 17), death_date: Date.new(1827, 3, 26), nationality: "German" },
+  { first_name: "Olivier", last_name: "Greif", birth_date: Date.new(1950, 1, 3), death_date: Date.new(2000, 5, 13), nationality_code: "fra" },
+  { first_name: "Sergueï", last_name: "Prokofiev", birth_date: Date.new(1891, 04, 11), death_date: Date.new(1953, 3, 5), nationality_code: "rus" },
 ]
 
 composers.each do |composer_data|
@@ -17,7 +17,7 @@ composers.each do |composer_data|
     death_date: composer_data[:death_date],
     short_bio: Faker::Lorem.sentence(word_count: 15),
     bio: Faker::Lorem.paragraph(sentence_count: 8, supplemental: true, random_sentences_to_add: 3),
-    nationality_id: Nationality.pluck(:id).sample
+    nationality: Nationality.find_by(code: composer_data[:nationality_code].upcase)
   )
   composer_count += 1
   puts "-- Created composer: #{Composer.last.first_name} #{Composer.last.last_name}"
