9614d3cc9823db0783b860cc6c4494b58b21d557
Thibaut Baissac <tbaissac@proton.me>
2025-06-21 15:11:23 +0200

    Conditional home page if only 1 composer present

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,5 +5,12 @@ class HomeController < ApplicationController
     @works_count = Work.count
     @movements_count = Movement.count
     @quotes_count = Quote.count
+
+    # If there's only one composer, make the page personalized
+    if @composers_count == 1
+      @composer = Composer.includes(:nationality, :works).first
+      @composer_works_count = @composer.works.count
+      @composer_quotes_count = @composer.quotes.count
+    end
   end
 end
